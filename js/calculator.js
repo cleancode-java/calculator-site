283c33aa0ba5ed99f311e33bee576ec3abbc4484
cleancode-java <sciencework1@gmail.com>
2026-01-15 20:57:16 +0000

    Update calculator.js
    changed URL, API without calculation logic that is transferred to the server

--- js/calculator.js
+++ js/calculator.js
@@ -28,8 +28,30 @@
 
 
 
-                const result = calculateTaxes(income, expenses, notes);
-                showResult(result);
+                const supabaseUrl = "https://ezixjoupqzlijyocuswx.supabase.co/functions/v1/Tax-calculator-function";
+                const supabaseKey = "sb_publishable_tXBcLSU0KidwZ8ZYFjetTg_FJRbRzXk";
+                const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+
+                try {
+                    const { data, error } = await supabase.functions.invoke('Tax-calculator-function', {
+                        body: { income, expenses, notes }
+                    });
+                    if (error) {
+                        $("#error").text("Server error: " + error.message);
+                        return;
+                    }
+                    // Якщо сервер повернув warning (наприклад, expenses > income)
+                    if (data.warning) {
+                        $("#error").text(data.warning);
+                    }
+                    // Показуємо результат
+                    showResult(data);
+                } catch (err) {
+                    console.error(err);
+                    $("#error").text("Server error. Please try again.");
+                }
+        
             });
 
         });
@@ -51,168 +73,6 @@
             return {valid: true};
         }
 
-        const personalAllowance = 12570;
-        const niLowerLimit = 12570;
-        const niUpperLimit = 50270;
-
-        const NI_BASIC_RATE = 0.06;
-        const NI_HIGHER_RATE = 0.02;
-
-        const PA_STANDARD = 12570;
-        const PA_TAPER_STANDART = 100000;
-        const PA_TAPER_END = 125140;
-
-        const IT_BASIC_LIMIT= 50270;
-        const IT_ADDITIONAL_LIMIT = 125140;
-        const IT_BASIC_RATE = 0.20;
-        const IT_HIGHER_RATE = 0.40;
-        const IT_ADDITIONAL_RATE = 0.45;
-
-
-        // function for caunting Personal Allowance with tapering
-
-        function calculatePersonalAllowance(profit) {
-            if (profit <= PA_TAPER_STANDART) return PA_STANDARD;
-            if (profit >= PA_TAPER_END) return 0;
-            const reduction = (profit - PA_TAPER_STANDART) / 2;
-            return Math.max(0, PA_STANDARD - reduction);
-        }
-
-        // Function Income Tax with breakdown
-        function calculateIncomeTax(profit) {
-            const personalAllowance = calculatePersonalAllowance(profit);
-            let remaining = Math.max (0, profit - personalAllowance);
-
-            const breakdown = {
-                zeroRate: {amount : personalAllowance, tax: 0},
-                basicRate: {amount: 0, tax: 0},
-                higherRate: {amount: 0, tax: 0},
-                additionalRate: {amount: 0, tax: 0}
-            };
-
-            // Basic rate 20%
-            const basicAmount = Math.min(remaining, IT_BASIC_LIMIT - personalAllowance);
-            breakdown.basicRate.amount = basicAmount;
-            breakdown.basicRate.tax = basicAmount * IT_BASIC_RATE;
-            remaining -= basicAmount;
-
-            // Higher rate 40%
-            if(remaining > 0){
-                const higherAmount = Math.min(remaining, IT_ADDITIONAL_LIMIT - IT_BASIC_LIMIT);
-                breakdown.higherRate.amount = higherAmount;
-                breakdown.higherRate.tax = higherAmount * IT_HIGHER_RATE;
-                remaining -= higherAmount;
-            }
-
-            // Additional rate 45%
-            if (remaining > 0) {
-                breakdown.additionalRate.amount = remaining;
-                breakdown.additionalRate.tax = remaining * IT_ADDITIONAL_RATE;
-            }
-
-            const total= breakdown.basicRate.tax + breakdown.higherRate.tax + breakdown.additionalRate.tax;
-
-            return {total, breakdown};
-        }
-
-
-
-
-
-        function calculateClass4NI (profit, rateBasic, rateHigher){
-            let ni = 0;
-            
-            if (profit <= niLowerLimit){
-                return {
-                    total: 0,
-                    breakdown: {
-                        zeroRate: {amount: profit, tax: 0},
-                        basicRate: {amount: 0, tax: 0},
-                        higherRate: {amount: 0, tax: 0}
-                    }
-                };
-            }
-
-            // in next part of code I calculate National Insurance basic rate 6% in the range of numbers from 12.570 to 50.270
-            // What does the Math.min function do? It takes the lesser of two (I have marked them in brackets)
-            // or the entire profit or the upper limit NI 50.270
-            // for example, if profit = 30.000 we take 30.000. If profit = 70.000 we take 50.270
-            // So, if profit 30.000, we 30.000-12.570=17.430; if profit 70.000, we 50.270-12.570=37.700 (in our case 12.570 it is niLowerLimit)
-            // I also wrote Math.max - If profit is below the NI lower limit, NI basic part should be 0. 
-            const basicPart = Math.max (0, Math.min (profit, niUpperLimit) - niLowerLimit);
-            const niBasicTax = basicPart * rateBasic;
-            ni = ni + niBasicTax;
-            // we can also write the previous two lines this way, for example:
-            // ni = ni + (basicPart * rateBasic);
-
-            // next part code with if - count NI if profit > niUpperLimit. It's tiered taxation:
-            // basicPart - it's only the middle range; higherPart - only the amount above the upper threshold. Thay never overlap on the same part
-            
-            let higherPart = 0;
-            let niHigherTax = 0;
-            if (profit > niUpperLimit) {
-                higherPart = profit - niUpperLimit;
-                niHigherTax = higherPart * rateHigher;
-                ni += niHigherTax;
-            }
-            return {
-                total: ni,
-                breakdown: {
-                    zeroRate: {
-                        amount: Math.min(profit, niLowerLimit),
-                        tax: 0
-                    },
-                    basicRate: {
-                        amount: basicPart,
-                        tax: niBasicTax
-                    },
-                    higherRate: {
-                        amount: higherPart,
-                        tax: niHigherTax
-                    }
-                }
-            };
-        }
-
-        // Main function of calculation
-        function calculateTaxes (income, expenses, notes){
-            const profit = income - expenses;
-
-            const incomeTaxData = calculateIncomeTax(profit);
-            const incomeTax = incomeTaxData.total;
-
-            const niData = calculateClass4NI (profit, NI_BASIC_RATE, NI_HIGHER_RATE);
-            const ni = niData.total;
-
-            const netIncome = profit - incomeTax - ni;
-
-            return {
-                profit,
-                incomeTax,
-                incomeTaxBreakdown: incomeTaxData.breakdown,
-                ni,
-                niBreakdown: niData.breakdown,
-                netIncome,
-                notes
-            };
-        }
-
-        // function to determine the effective marginal rate
-        // 0% - Personal Allowance
-        // 20% Income Tax + 6% NI
-        // 40% Income Tax + 2% NI
-        // 60% Trap
-        // return 47 = 45% Income Tax + 2%NI
-        function getEffectiveRate(profit) {
-            if (profit <= 12570) return 0; 
-            if (profit <= 50270) return 26;
-            if (profit <= 100000) return 42;
-            if (profit <=125140) {
-                // точна маржинальна ставка з taper Personal Allowance
-                return 40 + 0.5 * 40 + 2;
-            }
-            return 47;
-        }
 
         // функція виводу (showeResult)
         function showResult(data){
@@ -350,5 +210,6 @@ function updateText(id, value) {
     const el = document.getElementById(id);
     if (el) el.textContent = value.toFixed(2);
 }
+
 
         
