20eb4bf33bcb60072d8fd181aa3df4c62059776e
cleancode-java <sciencework1@gmail.com>
2026-01-18 15:51:03 +0000

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -32,7 +32,7 @@ async function callServer(income, expenses, notes) {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${sb_publishable_tXBcLSU0KidwZ8ZYFjetTg_FJRbRzXk}`
+          "Authorization": "Bearer sb_publishable_tXBcLSU0KidwZ8ZYFjetTg_FJRbRzXk"
         },
         body: JSON.stringify({ income, expenses, notes })
       }
@@ -208,6 +208,7 @@ function updateText(id, value) {
 
 
         
+
 
 
 
