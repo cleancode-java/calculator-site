82b96d968d8abfd2d3d251772f64664c16188708
cleancode-java <sciencework1@gmail.com>
2026-02-18 14:41:07 +0000

    Update main.js
    update all code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 // code for header
 document.addEventListener("DOMContentLoaded", () => {
     loadHeader();
+    // ====== Hamburger menu JS ======
+    // the code is executed after the header is already inserted into the DOM
+    const mobileMenuBtn = document.getElementById("mobileMenuBtn");
+    const mainNav = document.getElementById("mainNav");
+
+    mobileMenuBtn.addEventListener("click", () => {
+            console.log("Button clicked")
+    });
+
+    if (mobileMenuBtn && mainNav) {
+        mobileMenuBtn.addEventListener("click", () => {
+            mainNav.classList.toggle("hidden");
+        });
+    }
 });
 
 // imitate who is it: guest, userFree, userPro
@@ -20,41 +34,68 @@ function loadHeader(){
     }
 
     highlightActiveNav();
+    initLogout();
+}
+
+
+
+function initLogout(){
+
+    
 }
 
 // Guest header
 function guestHeader(){
     return `
        <header class="bg-gray-200">
-        <section class="bg-white shadow-md">
+        <section class="bg-white shadow-md shadow-emerald-700/20">
             <div class="max-w-7xl mx-auto px-6 py-8
                         flex flex-col gap-6
                         md:flex-row md:items-center md:justify-between">
-                <div class="max-w-2xl">
-                    <h1 class="text-3xl font-bold mb-3">
-                        FinLoggy: UK Tax Calculator for Self-Employed
+                <div class="max-w-2xl border-l-4 border-emerald-600 pl-4">
+                    <h1 class="text-3xl font-bold mb-2">
+                    <a href="/" class="text-3xl font-bold hover:opacity-80">
+                        <span class="text-emerald-900">Fin</span><span class="text-emerald-700">Loggy</span>
+                    </a>  
                     </h1>
                     <p class="text-gray-600">
+                        UK tax calculator for sole traders and freelancers.
+                    </p>
+                    <p class="text-sm text-gray-500 mt-1">
                         Estimate your Income Tax and National Insurance contributions.
-                        Designed for UK sole traders and freelancers.
                     </p>
                 </div>
 
-                <div class="navigation">
-                    <nav class="space-x-6 text-sm md:justify-end">
-                        <ul class="flex gap-2">
-                            <li><a href="/" class="px-3 py-1.5 hover:underline">Home</a></li>
-                            <li><a href="why-choose-pro.html" class="px-3 py-1.5 hover:underline">Why choose Pro</a></li>
-                            <li><a href="how-it-works.html" class="px-3 py-1.5 hover:underline">How it works</a></li>
-                            <li><a href="contact-us.html" class="px-3 py-1.5 hover:underline">Contact us</a></li>
+                <div class="navigation flex items-center gap-4">
+                    <button id="mobileMenuBtn"
+                            class="md:hidden text-3xl font-bold">
+                        ☰
+                    </button>
+
+                    <!-- Navigation -->
+                    <nav id="mainNav" class="hidden md:block text-sm">
+                        <ul class="flex flex-col gap-2 md:flex-row md:flex-wrap">
+                            <li><a href="/" class="px-3 py-1.5 text-gray-700 hover:text-emerald-700 transition">Home</a></li>
+                            <li><a href="why-choose-pro.html" class="px-3 py-1.5 text-gray-700 hover:text-emerald-700 transition">Why choose Pro</a></li>
+                            <li><a href="how-it-works.html" class="px-3 py-1.5 text-gray-700 hover:text-emerald-700 transition">How it works</a></li>
+                            <li><a href="contact-us.html" class="px-3 py-1.5 text-gray-700 hover:text-emerald-700 transition">Contact us</a></li>
+                            <li><a href="login.html" 
+                                class="px-3 py-1.5 text-emerald-700 font-semibold hover:text-emerald-900 transition">
+                                Login
+                            </a>
+                            </li>
                         </ul>
                     </nav>
+
                 </div>
             </div>
         </section>
     </header>
     `;
 }
+
+
+
 
 // Free header (поки пустий, можу додавати додаткові меню)
 function freeHeader(){
@@ -81,7 +122,11 @@ function freeHeader(){
                             <li><a href="/why-choose-pro.html" class="px-3 py-1.5 hover:underline">Calculator</a></li>
                             <li><a href="how-it-works.html" class="px-3 py-1.5 hover:underline">Upgrade to Pro</a></li>
                             <li><a href="contact-us.html" class="px-3 py-1.5 hover:underline">Contact us</a></li>
-                            <li><a href="contact-us.html" class="px-3 py-1.5 hover:underline">Logout</a></li>
+                            <li><button id="logoutBtn" 
+                                class="px-3 py-1.5 text-red-600 hover:text-red-800 transition">
+                                Logout
+                            </button>
+                            </li>
                         </ul>
                     </nav>
                 </div>
@@ -117,7 +162,11 @@ function proHeader(){
                             <li><a href="#" class="px-3 py-1.5 hover:underline">Reports</a></li>
                             <li><a href="#" class="px-3 py-1.5 hover:underline">Analitics</a></li>
                             <li><a href="contact-us.html" class="px-3 py-1.5 hover:underline">Contact us</a></li>
-                            <li><a href="contact-us.html" class="px-3 py-1.5 hover:underline">Logout</a></li>
+                            <li><button id="logoutBtn" 
+                                class="px-3 py-1.5 text-red-600 hover:text-red-800 transition">
+                                Logout
+                            </button>
+                            </li>
                         </ul>
                     </nav>
                 </div>
@@ -157,34 +206,6 @@ document.addEventListener("DOMContentLoaded", ()=> {
     }
 });
 
-
-
-/*
-// Вставляю появу модального вікна для кнопки Upgrade Pro, оскільки кнопка ще не готова
-// Отримуємо елементи
-const upgradeBtn = document.getElementById("upgradeBtn");
-const modal = document.getElementById("modal");
-const closeModal = document.getElementById("closeModal");
-
-// Відкриваємо модальне вікно при натисканні Upgrade
-upgradeBtn.addEventListener("click", () => {
-    modal.classList.remove("hidden");
-});
-
-// Закриваємо модальне вікно при натисканні OK
-closeModal.addEventListener("click", () => {
-    modal.classList.add("hidden");
-});
-
-// Додатково: закриття при кліку поза вікном
-modal.addEventListener("click", (e) => {
-    if (e.target === modal) {
-        modal.classList.add("hidden");
-    }
-});
-
-*/
-
 const upgradeBtn = document.getElementById("upgradeBtn");
 const modal = document.getElementById("modal");
 const closeModal = document.getElementById("closeModal");
@@ -208,4 +229,14 @@ if (upgradeBtn && modal && closeModal) {
     }
   });
 
+}
+
+
+const logoutBtn = document.getElementById("logoutBtn");
+
+if (logoutBtn) {
+  logoutBtn.addEventListener("click", async () => {
+    await supabase.auth.signOut();
+    window.location.href = "/";
+  });
 }
