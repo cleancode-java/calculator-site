4f424b2121be7a77f7f5fbf0369bd7a5e0a47af3
cleancode-java <sciencework1@gmail.com>
2026-03-02 02:32:54 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,21 +48,22 @@ function initLogout(){
 function guestHeader(){
     return `
        <header class="bg-gray-200">
-        <section class="bg-white shadow-md shadow-emerald-700/20">
+        <section class="bg-white shadow-md shadow-fuchsia-700/20">
             <div class="max-w-7xl mx-auto px-6 py-8
                         flex flex-col gap-6
                         md:flex-row md:items-center md:justify-between">
-                <div class="max-w-2xl border-l-4 border-emerald-600 pl-4">
+                <div class="max-w-2xl border-l-4 border-fuchsia-600 pl-4">
                     <h1 class="text-3xl font-bold mb-2">
                     <a href="/" class="text-3xl font-bold hover:opacity-80">
-                        <span class="text-emerald-900">Fin</span><span class="text-emerald-700">Loggy</span>
-                    </a>  
+                        <span class="text-purple-800">Acci</span><span class="text-fuchsia-600">Vell</span>
+                    </a>
                     </h1>
-                    <p class="text-gray-600">
-                        UK tax calculator for sole traders and freelancers.
+                    <p class="text-lg text-gray-800 font-medium">
+                        Numbers that empower your decisions.
                     </p>
                     <p class="text-sm text-gray-500 mt-1">
-                        Estimate your Income Tax and National Insurance contributions.
+                        Accivell is a simple finance tool built for self-employed professionals who want clarity,
+                         confidence, and control over their numbers.
                     </p>
                 </div>
 
@@ -75,12 +76,12 @@ function guestHeader(){
                     <!-- Navigation -->
                     <nav id="mainNav" class="hidden md:block text-sm">
                         <ul class="flex flex-col gap-2 md:flex-row md:flex-wrap">
-                            <li><a href="/" class="px-3 py-1.5 text-gray-700 hover:text-emerald-700 transition">Home</a></li>
-                            <li><a href="why-choose-pro.html" class="px-3 py-1.5 text-gray-700 hover:text-emerald-700 transition">Why choose Pro</a></li>
-                            <li><a href="how-it-works.html" class="px-3 py-1.5 text-gray-700 hover:text-emerald-700 transition">How it works</a></li>
-                            <li><a href="contact-us.html" class="px-3 py-1.5 text-gray-700 hover:text-emerald-700 transition">Contact us</a></li>
+                            <li><a href="/" class="px-3 py-1.5 text-gray-700 hover:text-fuchsia-700 transition">Home</a></li>
+                            <li><a href="why-choose-pro.html" class="px-3 py-1.5 text-gray-700 hover:text-fuchsia-700 transition">Why choose Pro</a></li>
+                            <li><a href="how-it-works.html" class="px-3 py-1.5 text-gray-700 hover:text-fuchsia-700 transition">How it works</a></li>
+                            <li><a href="contact-us.html" class="px-3 py-1.5 text-gray-700 hover:text-fuchsia-700 transition">Contact us</a></li>
                             <li><a href="login.html" 
-                                class="px-3 py-1.5 text-emerald-700 font-semibold hover:text-emerald-900 transition">
+                                class="px-3 py-1.5 text-purple-800 font-semibold hover:text-fuchsia-700 transition">
                                 Login
                             </a>
                             </li>
@@ -205,6 +206,8 @@ document.addEventListener("DOMContentLoaded", ()=> {
         yearEl.textContent = new Date().getFullYear();
     }
 });
+
+
 
 const upgradeBtn = document.getElementById("upgradeBtn");
 const modal = document.getElementById("modal");
